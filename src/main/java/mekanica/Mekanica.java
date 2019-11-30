ab692b11dc1e6ac2851f7948701ef2d7155fcf15
Dave Smith <dizzyd@dizzyd.com>
2019-11-30 10:17:07 -0700

    Sketch out main class

--- src/main/java/mekanica/Mekanica.java
+++ src/main/java/mekanica/Mekanica.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package mekanica;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Mekanica.MODID, name = Mekanica.NAME, version = Mekanica.VERSION)
+public class Mekanica
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "mekanica";
+    public static final String NAME = "Mekanica";
+    public static final String VERSION = "0.0.0";
 
     private static Logger logger;
 
@@ -25,7 +25,5 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
